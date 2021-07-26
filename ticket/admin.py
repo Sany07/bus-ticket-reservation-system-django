89d991bb3dd7fc88812e501c92216775eda4e130
Ahasan Habib Sany <46066881+Sany07@users.noreply.github.com>
2021-07-26 16:16:24 +0600

    model fields changed

--- ticket/admin.py
+++ ticket/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from ticket.models import *
+
+
+@admin.register(TicketPrice)
+class TicketPriceAdmin(admin.ModelAdmin):
+    list_display = [
+        # "trip_route",
+        "fleet_type",
+        "price",
+        "updated_at"
+
+    ]
+    # list_filter = ["trip_route"]
--- ticket/models.py
+++ ticket/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from fleet_management.models import FleetType
+from trip_management.models import Route
+
+
+class TicketPrice(models.Model):
+    trip_route = models.ForeignKey(Route, related_name="ticket_trip_route", on_delete=models.CASCADE)
+    fleet_type = models.ForeignKey(FleetType,related_name="ticket_fleet_type", on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    updated_at = models.DateTimeField(auto_now=True)
+        
+
+    class Meta:
+        verbose_name = "Ticket Price"
+        verbose_name_plural = "Ticket Prices"
+
+    def __str__(self):
+        return f'{self.trip_route} {self.price}'
+
+    # def get_absolute_url(self):
+    #     return reverse("TicketPrice_detail", kwargs={"pk": self.pk})
